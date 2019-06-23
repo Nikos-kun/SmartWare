4b1dcfa943c6c7fc8c83199f0c274aec0d2dedae
NikosAthanasiadis <nikosathanasiadis1990@hotmail.com>
2019-06-23 23:41:49 +0300

    Deleted "Proxeiro.js" + "main.html"
    Deleted two files that i don't need anymore. I place the code from main.html into the index.html, inside the nd-view. So I can see the menu as soon as the app starts.

--- app.js
+++ app.js
@@ -5,30 +5,43 @@
 	myApp.config(function ($routeProvider) {
 		$routeProvider
 
-			// route for the home page
-			.when('/pelates', {
-				templateUrl: 'partials/pelates.html',
-				controller: 'PelatesCtrl'
+			// route for the menu page
+			.when('index', {
+				templateUrl: 'index.html',
+				controller: 'IndexCtrl'
 			})
 
-			// route for the about page
+			// route for the menu page
 			.when('/main', {
 				templateUrl: 'partials/main.html',
 				controller: 'HeaderCtrl'
+			})
+
+			// route for the pelates page
+			.when('/pelates', {
+				templateUrl: 'partials/pelates.html',
+				controller: 'PelatesCtrl'
 			});
 	});
 
 	// create the controller and inject Angular's $scope
-	myApp.controller("HeaderCtrl", function ($scope) {
+	myApp.controller("IndexCtrl", function ($scope) {
 		// create a message to display in our view
 		$scope.appDetails = {};
-		$scope.appDetails.title = "Μενού";
+		$scope.appDetails.menu = "Μενού";
+	});
+
+	// create the controller and inject Angular's $scope
+	myApp.controller("MainCtrl", function ($scope) {
+		// create a message to display in our view
+		$scope.appDetails = {};
+		$scope.appDetails.menu = "Μενού";
 	});
 
 	myApp.controller('PelatesCtrl', function ($scope, $http, $log) {
 
 		$scope.appDetails = {};
-		$scope.appDetails.title = "Επιλέξτε Πελάτη";
+		$scope.appDetails.pelates = "Επιλέξτε Πελάτη";
 
 		var successCallBack = function (response) {
 
